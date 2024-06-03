7e4cf18d5935159b49cd6e728415fc446d18cb21
Sima choudhary <143160371+Sima-Choudhary@users.noreply.github.com>
2024-06-03 21:57:17 +0530

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
-import Footer from './components/Footer.js';
 import Frame1 from './components/Frame1.js';
 import Frame2 from './components/Frame2.js';
 import Frame3 from './components/Frame3.js';
 import Frame4 from './components/Frame4.js';
 import Frame5 from './components/Frame5.js';
 import Frame6 from './components/Frame6.js';
+import Frame7 from './components/Frame7.js';
 
 
 function App() {
@@ -18,7 +18,8 @@ function App() {
       <Frame4/>
       <Frame5/>
       <Frame6/>
-      <Footer/>
+      <Frame7/>
+      
      </div>
   );
 }
---
+++ src/components/Frame7.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Form7.css'
+export default function Frame7() {
+  return (
+    <div>
+      <div id='footer'>
+      <div>
+          <h1>Gyan Deep Computer Education</h1>
+          <p>Morbi ac sit vel nisl. Scelerisque viverra tempus tortor facilisis magna.
+             Ultricies suspendisse a libero lorem sodales eget.</p>
+             <div></div>
+      </div>
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+      <div></div>
+      </div>
+    </div>
+  )
+}
